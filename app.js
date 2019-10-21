20512b10d9f0735190e4bb4b719fffaca9429618
Rafael Faria Carvalho <rafael.carvalho@programmers.com.br>
2019-10-21 16:49:06 -0300

    delete

--- app.js
+++ app.js
@@ -1,36 +1,97 @@
-const express = require('express')
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const Product = require('./product.js');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const Product = require("./product.js");
 
 var app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-mongoose.connect(
-    'mongodb://localhost:27017/http_client',
-    {useNewUrlParser: true}
-);
+mongoose.connect("mongodb://localhost:27017/http_client", {
+  useNewUrlParser: true
+});
 
 var myLogger = function(req, res, next) {
-    console.log(req.body);
-    next();
-}
+  console.log(req.body);
+  next();
+};
 app.use(myLogger);
 
 // localhost:9000/products
-app.get('/products', function(req, res) {
-    Product.find().lean().exec(
-       (err, prods) => {
-           if (err) 
-            res.status(500).send(err);
-           else
-            res.status(200).send(prods);
-       } 
-    );
+app.get("/products", function(req, res) {
+  Product.find()
+    .lean()
+    .exec((err, prods) => {
+      if (err) res.status(500).send(err);
+      else res.status(200).send(prods);
+    });
 });
+
+app.get("/productserr", function(req, res) {
+  setTimeout(() => {
+    res.status(500).send({ msg: "Error message from the server" });
+  }, 2000);
+});
+
+app.get("/productsdelay", function(req, res) {
+  setTimeout(() => {
+    Product.find()
+      .lean()
+      .exec((err, prods) => {
+        if (err) res.status(500).send(err);
+        else res.status(200).send(prods);
+      });
+  }, 2000);
+});
+
+app.get("/products_ids", function(req, res) {
+    Product.find()
+      .lean()
+      .exec((err, prods) => {
+        if (err) res.status(500).send(err);
+        else res.status(200).send(prods.map(p=>p._id));
+      });
+});
+
+app.get("/products/name/:id", function(req, res) {
+    const id = req.params.id;
+    Product.findById(id,
+        (err, prod) => {
+        if (err)
+            res.status(500).send(err);
+        if (!prod)
+            res.status(404).send({});
+        else
+            res.status(200).send(prod.name);
+      });
+});
+
+app.post('/products', function(req, res){
+    p = new Product({
+        name: req.body.name,
+        price: req.body.price,
+        department: req.body.price
+    });
+    p.save((err,prod) => {
+        if(err)
+            res.status(500).send(err);
+        else
+            res.status(200).send(prod);    
+    });
+});
+
+app.delete('/products/:id', function(req, res){
+    Product.deleteOne({_id: req.params.id},
+        (err) => {
+            if(err)
+                res.status(500).send(err);
+            else  
+                res.status(200).send({});              
+        })
+});
+
 
 // Seta a porta que ele vai rodar
 app.listen(3000);
